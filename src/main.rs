fba6127cab75fecd5c1ac15488e26ab6c8059521
girordo <tsgiroldo@gmail.com>
2023-07-17 01:28:13 -0300

    feat(telemetry): pag 116

--- src/main.rs
+++ src/main.rs
@@ -26,18 +26,9 @@ pub fn init_subscriber(subscriber: impl Subscriber + Send + Sync) {
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    LogTracer::init().expect("Failed to set logger");
+    let subscriber = get_subscriber("zero2prod".into(), "info".into());
 
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-
-    let formatting_layer = BunyanFormattingLayer::new("zero2prod".into(), std::io::stdout);
-
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-
-    set_global_default(subscriber).expect("Failed to set subscriber");
+    init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration");
 
